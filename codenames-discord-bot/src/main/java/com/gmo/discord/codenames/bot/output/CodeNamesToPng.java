5ab19fd2c5326c48ca2ab9c53ac70b6cea9f6c9f
Tyler Edelen <tyler.edelen@gmail.com>
2020-10-31 19:10:53 -0400

    fix text rendering

--- codenames-discord-bot/src/main/java/com/gmo/discord/codenames/bot/output/CodeNamesToPng.java
+++ codenames-discord-bot/src/main/java/com/gmo/discord/codenames/bot/output/CodeNamesToPng.java
@@ -1,12 +1,17 @@
 package com.gmo.discord.codenames.bot.output;
 
+import com.gmo.discord.codenames.bot.entities.Card;
+import com.gmo.discord.codenames.bot.entities.GameBoard;
+import com.gmo.discord.codenames.bot.entities.TeamType;
+import com.gmo.discord.codenames.bot.store.RandomWordsFromFileMapSupplier;
+
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.font.TextAttribute;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
@@ -15,13 +20,7 @@ import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.HashMap;
 import java.util.Map;
-
-import com.gmo.discord.codenames.bot.entities.Card;
-import com.gmo.discord.codenames.bot.entities.GameBoard;
-import com.gmo.discord.codenames.bot.entities.TeamType;
-import com.gmo.discord.codenames.bot.store.RandomWordsFromFileMapSupplier;
 
 public class CodeNamesToPng {
     public static final CodeNamesToPng INSTANCE = new CodeNamesToPng();
@@ -33,12 +32,12 @@ public class CodeNamesToPng {
     private static final int WIDTH = (CARD_WIDTH + SPACING) * 5 + SPACING;
     private static final int ARC_SIZE = 30;
     private static final int FONT_SIZE = 22;
-    private static final int CHARACTER_WIDTH = 18;
+    private static final int CHARACTER_WIDTH = 17;
     private static final Font FONT;
 
     static {
         final Font fontBig = new Font(Font.MONOSPACED, Font.PLAIN, FONT_SIZE);
-        FONT = fontBig.deriveFont(Map.of(TextAttribute.TRACKING, 0.15,
+        FONT = fontBig.deriveFont(Map.of(TextAttribute.TRACKING, 0.125,
                 TextAttribute.WIDTH, TextAttribute.WIDTH_SEMI_EXTENDED));
     }
 
@@ -68,19 +67,22 @@ public class CodeNamesToPng {
 
         @Override
         public void paintComponent(final Graphics ig2) {
-            ig2.setFont(FONT);
+            final Graphics2D g2d = (Graphics2D) ig2;
+            g2d.setFont(FONT);
+            g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+            g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
             for (int i = 0; i < 5; i++) {
                 for (int j = 0; j < 5; j++) {
                     final Card c = map[i][j];
                     final Color color = getColor(c, showAll);
-                    ig2.setColor(color);
+                    g2d.setColor(color);
                     final int topLeftX = j * (CARD_WIDTH + SPACING) + SPACING;
                     final int topLeftY = i * (CARD_HEIGHT + SPACING) + SPACING;
-                    ig2.fillRoundRect(topLeftX, topLeftY, CARD_WIDTH, CARD_HEIGHT, ARC_SIZE, ARC_SIZE);
-                    ig2.setColor(color == Color.BLACK ? Color.WHITE : Color.BLACK);
+                    g2d.fillRoundRect(topLeftX, topLeftY, CARD_WIDTH, CARD_HEIGHT, ARC_SIZE, ARC_SIZE);
+                    g2d.setColor(color == Color.BLACK ? Color.WHITE : Color.BLACK);
                     final int centerX = topLeftX + (CARD_WIDTH / 2) + SPACING;
                     final int offset = (int)(((double)c.getWord().length() / 2) * CHARACTER_WIDTH);
-                    ig2.drawString(c.getWord(), centerX - offset, topLeftY + (CARD_HEIGHT / 2) + SPACING);
+                    g2d.drawString(c.getWord(), centerX - offset, topLeftY + (CARD_HEIGHT / 2) + SPACING);
                 }
             }
         }
@@ -109,7 +111,7 @@ public class CodeNamesToPng {
                 new RandomWordsFromFileMapSupplier("words.txt").get(),
                 TeamType.RED);
         board.getGameMap()[0][1].reveal();
-        final byte[] pngBytes = CodeNamesToPng.INSTANCE.getPngBytes(board.getGameMap(), false);
-        Files.write(Paths.get("/Users/tedelen/Downloads/yomomma.png"), pngBytes, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        final byte[] pngBytes = CodeNamesToPng.INSTANCE.getPngBytes(board.getGameMap(), true);
+        Files.write(Paths.get("/Users/tyler/Downloads/yomomma.png"), pngBytes, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
     }
 }
